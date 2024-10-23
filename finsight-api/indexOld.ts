30851c9d81149c60a527504177f0eb4a65cb8c76
JZ2803 <jessica.zheng67@gmail.com>
2024-10-24 01:08:48 +1100

    update index.ts file

---
+++ finsight-api/indexOld.ts
@@ -0,0 +1,10 @@
+// import app from "./app";
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, (err) => {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(`Server is running on port ${PORT}`);
+//     }
+// });
